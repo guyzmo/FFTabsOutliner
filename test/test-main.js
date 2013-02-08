b0b75c5a6e9b74dd00f09286e15edc3360442be0
Guyzmo <guyzmo+github@m0g.net>
2013-02-08 12:54:04 +0100

    added tests

--- test/test-main.js
+++ test/test-main.js
@@ -2,6 +2,29 @@ var main = require("main");
 
 exports["test main"] = function(assert) {
   assert.pass("Unit test running!");
+
+  var root = new main.TNode(null);
+
+  root.add("waf");
+  root.add("meaw");
+  root.add("waf");
+  root.add("coin");
+
+  root.get("coin").add("quack");
+  root.get("coin").add("cotcot");
+  root.get("coin").add("coocoo");
+
+  root.get("coin").get("coocoo").add("squeak");
+
+  assert.pass(root.has("waf") === true);
+  assert.pass(root.has("coin") === true);
+  assert.pass(root.has("coocoo") === true);
+  assert.pass(root.has("squeak") === false);
+  assert.pass(root.get("coocoo").has("squeak") === true);
+
+  //root.print();
+  //root.get("coin").print();
+
 };
 
 exports["test main async"] = function(assert, done) {
